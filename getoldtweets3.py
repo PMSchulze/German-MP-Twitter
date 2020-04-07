a657c4eb716c42069a8429a8b07138be8309a73a
Patrick Schulze <Pa.Schulze@campus.lmu.de>
2020-04-07 16:59:10 +0200

    update twitter scraping

--- getoldtweets3.py
+++ getoldtweets3.py
@@ -30,28 +30,34 @@ def get_twitter_url(x):
         return ''
 url = bt_data['Soziale Medien'].apply(get_twitter_url)
 twitter_account = pd.concat([bt_data['Name'], url], axis = 1, \
-                            keys = ['Name','Twitter Url'])
+                            keys = ['Name','Url'])
 #-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
+# function to get twitter usernames from twitter url
 def get_twitter_username(url):
     if url:
         return(url.split('/')[3].split('?')[0])
     else: 
         return('')
-    
+
+# function to download tweets for a specific user 
 def download_tweets(username):
     print(f"Downloading for {username}")
     tweetCriteria = got.manager.TweetCriteria().setUsername(username)\
-                                               .setSince("2020-03-04")\
-                                               .setUntil("2020-06-04")\
+                                               .setSince("2017-09-24")\
+                                               .setUntil("2020-04-07")
 
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
     df = pd.DataFrame([tweet.__dict__ for tweet in tweets])
     return(df)
 
-twitter_usernames = twitter_account['Twitter Url'].apply(get_twitter_username)
 
-df = pd.DataFrame()
-for username in twitter_usernames[0:10]:
-    df = pd.concat([df, download_tweets(username)])
+twitter_usernames = twitter_account['Url'].apply(get_twitter_username)
+twitter_usernames.rename("Username", inplace = True)
+twitter_account = pd.concat([twitter_account, twitter_usernames], axis = 1)
+    
+res = pd.DataFrame()
+for username in twitter_usernames[0:3]:
+    res = pd.concat([res, download_tweets(username)])
+#-----------------------------------------------------------------------------
