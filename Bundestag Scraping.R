0bd62c53f4082dec1ecda17b88f7f85248b9871e
pauliko <pauliko@cip.ifi.lmu.de>
2020-04-08 15:21:31 +0200

    Zieht alle Twitter Links von der Fraktionsseite der CDU

--- Bundestag Scraping.R
+++ Bundestag Scraping.R
@@ -1,4 +1,21 @@
 
-#Bundestagseite von CDU Abgeordneten scrapen
 
-x <- 5 + 5
+library(plyr)
+library(tidyverse)
+library(magrittr)
+library(rvest)
+library(XML)
+library(stringi)
+library(pbapply)
+
+#Twitter Namen von CDU Fraktionsseite scrapen
+
+landing_page <- read_html("https://www.cducsu.de/hier-stellt-die-cducsu-bundestagsfraktion-ihre-abgeordneten-vor")
+
+
+partei2 <- landing_page %>% 
+  html_nodes("li.twitter > a") %>% html_attr("href") %>% 
+  as_tibble()
+
+
+
