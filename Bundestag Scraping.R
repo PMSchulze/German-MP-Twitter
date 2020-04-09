66fbd4487dae705506eb787683b4bb89ceae9bc8
pauliko <pauliko@cip.ifi.lmu.de>
2020-04-09 11:03:29 +0200

    Twitternamen von CDUCSU, SPD und FDP

--- Bundestag Scraping.R
+++ Bundestag Scraping.R
@@ -6,16 +6,50 @@ library(magrittr)
 library(rvest)
 library(XML)
 library(stringi)
+library(stringr)
 library(pbapply)
 
 #Twitter Namen von CDU Fraktionsseite scrapen
 
-landing_page <- read_html("https://www.cducsu.de/hier-stellt-die-cducsu-bundestagsfraktion-ihre-abgeordneten-vor")
+CDUSeite <- read_html("https://www.cducsu.de/hier-stellt-die-cducsu-bundestagsfraktion-ihre-abgeordneten-vor")
 
 
-partei2 <- landing_page %>% 
-  html_nodes("li.twitter > a") %>% html_attr("href") %>% 
-  as_tibble()
+CDU <- CDUSeite %>% 
+  html_nodes("li.twitter > a") %>% html_attr("href", default = "NA") %>% 
+  as_tibble() 
+
+NamenCDU <- CDU %>% mutate(value = str_replace(value, "http://twitter.com/", ""))
+NamenCDU <- NamenCDU %>%  mutate(value = str_replace(value, "https://twitter.com/", ""))
+NamenCDU <- NamenCDU %>% mutate(value = str_replace(value, "http://www.twitter.com/", ""))
+
+
+SPDSeite <- read_html("https://www.spdfraktion.de/abgeordnete/alle?wp=19&view=list&old=19")
+
+
+SPD <- SPDSeite %>% 
+  html_nodes("li > a.ico_twitter") %>% html_attr("href", default = "NA") %>% 
+  as_tibble() 
+
+NamenSPD <- SPD %>%mutate(value = str_replace(value, "http://twitter.com/", ""))
+NamenSPD <- NamenSPD %>% mutate(value = str_replace(value, "https://twitter.com/", ""))
+NamenSPD <- NamenSPD %>% mutate(value = str_replace(value, "http://www.twitter.com/", ""))
+
+
+
+FDPSeite <- read_html("https://www.fdpbt.de/fraktion/abgeordnete")
+FDP <- FDPSeite %>% 
+  html_nodes("div > a.tw") %>% html_attr("href", default = "NA") %>% 
+  as_tibble() 
+NamenFDP <- FDP %>%mutate(value = str_replace(value, "http://twitter.com/", ""))
+NamenFDP <- NamenFDP %>% mutate(value = str_replace(value, "https://twitter.com/", ""))
+NamenFDP <- NamenFDP %>% mutate(value = str_replace(value, "http://www.twitter.com/", ""))
+NamenFDP <- NamenFDP %>% mutate(value = str_replace(value, "https://www.twitter.com/", ""))
+
+
+
+
+
+
 
 
 
