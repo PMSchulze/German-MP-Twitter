af137d7e94b51caa964603b119c96e6cc155165d
Patrick Schulze <Pa.Schulze@campus.lmu.de>
2020-04-24 10:29:48 +0200

    change output if user cannot be downloaded

--- tweepy_helpers.py
+++ tweepy_helpers.py
@@ -6,6 +6,7 @@ Created on Tue Apr 14 14:02:51 2020
 @author: patrickschulze
 """
 import math
+import numpy as np
 import tweepy
 import pandas as pd
 
@@ -40,24 +41,23 @@ def get_hashtags(x):
 def download_tweets_tweepy(username):
     #initialize a list to hold all the tweepy Tweets
     alltweets = []
+    #specify relevant columns
+    colnames = ['created_at', 'full_text', 'in_reply_to_user_id_str','is_quote_status', \
+                'retweet_count', 'favorite_count', 'favorited', 'retweeted_status', \
+                'is_retweet', 'retweet_full_text', 'followers_count', 'location', 'hashtags']
     try:	
         #make initial request for most recent tweets (200 is the maximum allowed count)
-        new_tweets = api.user_timeline(screen_name = username,count=200,tweet_mode="extended")
-	
+        new_tweets = api.user_timeline(screen_name = username,count=200,tweet_mode="extended")	
         #save most recent tweets
-        alltweets.extend(new_tweets)
-	
+        alltweets.extend(new_tweets)	
         #save the id of the oldest tweet less one
-        oldest = alltweets[-1].id - 1
-	
+        oldest = alltweets[-1].id - 1	
         #keep grabbing tweets until there are no tweets left to grab
         while len(new_tweets) > 0:		
-            #all subsiquent requests use the max_id param to prevent duplicates
-            new_tweets = api.user_timeline(screen_name = username,count=200,max_id=oldest,tweet_mode="extended")
-		
+            #all subsequent requests use the max_id param to prevent duplicates
+            new_tweets = api.user_timeline(screen_name = username,count=200,max_id=oldest,tweet_mode="extended")		
             #save most recent tweets
-            alltweets.extend(new_tweets)
-		
+            alltweets.extend(new_tweets)		
             #update the id of the oldest tweet less one
             oldest = alltweets[-1].id - 1
         #convert output to pandas DataFrame	
@@ -66,8 +66,8 @@ def download_tweets_tweepy(username):
         outtweets['is_retweet'] = outtweets['retweeted_status'].apply(is_rewteet)
         #retrieve full_text if tweet is retweet
         outtweets['retweet_full_text'] = [outtweets['retweeted_status'][i].full_text \
-                                  if outtweets['is_retweet'][i] \
-                                      else '' for i in range(outtweets.shape[0])]
+                                          if outtweets['is_retweet'][i] \
+                                              else '' for i in range(outtweets.shape[0])]
         #retrieve number of followers of user
         outtweets['followers_count'] = [x.followers_count for x in outtweets['author']]
         #retrieve location
@@ -75,16 +75,15 @@ def download_tweets_tweepy(username):
         #retrieve hashtags as list
         outtweets['hashtags'] = outtweets['entities'].apply(get_hashtags)
         #store only relevant columns
-        colnames = ['created_at', 'full_text', 'in_reply_to_user_id_str','is_quote_status', \
-                    'retweet_count', 'favorite_count', 'favorited', 'retweeted_status', \
-                    'is_retweet', 'retweet_full_text', 'followers_count', 'location', 'hashtags']
         outtweets = outtweets[colnames]
-        #add boolean column for availability
+        #add boolean column for availability (True)
         outtweets.insert(0, 'available', True)
-        #add column with username
-        outtweets.insert(0, 'username', username)
     except:
         print('data for user %s cannot be downloaded' %username)
-        outtweets = pd.DataFrame([username], columns=['username'])
-        outtweets.insert(1, 'available', False)
+        #initialize DataFrame with row full of NaNs
+        outtweets = pd.DataFrame(np.nan, index = [0], columns = colnames)
+        #add boolean column for availability (False)
+        outtweets.insert(0, 'available', False)
+    #add column with username
+    outtweets.insert(0, 'username', username)
     return(outtweets)
