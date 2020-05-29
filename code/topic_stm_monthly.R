19098a78ec94932f1057a3e18b8a53c9da744823
simonwiegrebe1992 <simon.wiegrebe@gmail.com>
2020-05-29 15:19:20 -0300

    updated searchK iterations

--- code/topic_stm_monthly.R
+++ code/topic_stm_monthly.R
@@ -26,8 +26,9 @@ setwd('C:\\Users\\Simon\\OneDrive\\Uni\\LMU\\SS 2020\\Statistisches Consulting\\
 # setwd("/Users/patrickschulze/Desktop/Consulting/Bundestag-MP-Analyse")
 
 # ----------------------------------------------------------------------------------------------
-# ---------------------------------------------- STM -------------------------------------------
+# ---------------------------------- Hyperparameter Search -------------------------------------
 # ----------------------------------------------------------------------------------------------
+
 # load data
 data <- readRDS("./data/preprocessed_monthly.rds")
 colnames_table <- read.csv(file = "./data/topic_monthly_colnames.csv")
@@ -38,28 +39,32 @@ docs <- data$documents
 vocab <-  data$vocab
 meta <- data$meta
 
-# # search hyperparameter space for optimal K
-# hyperparameter_search <- searchK(
-#   documents = docs,
-#   vocab = vocab,
-#   data = meta,
-#   K = c(5,6,7,8,9,10),
-#   prevalence =~ Partei + Bundesland,
-#   max.em.its = 50,
-#   init.type = "Spectral"
-# )
+# search hyperparameter space for optimal K
+hyperparameter_search <- searchK(
+  documents = docs,
+  vocab = vocab,
+  data = meta,
+  # K = c(5,6,7,8,9,10),
+  K = c(5,10,15,20,25,30,35,40),
+  prevalence =~ Partei,
+  max.em.its = 150,
+  init.type = "Spectral"
+)
 
-# topic prevalence --------------------------------------------------------
+saveRDS(hyperparameter_search, "./data/searchK_large_data.rds")
+searchK_data <- readRDS("./data/searchK_large_data.rds")
+plot(searchK_data)
+
+# ----------------------------------------------------------------------------------------------
+# ---------------------------------- Prevalence Model Fitting ----------------------------------
+# ----------------------------------------------------------------------------------------------
 
 # choose covariates and number of topics
-prev_var <- c("Partei", "Bundesland")
-# prev_var <- c("Partei*Bundesland", "Partei*s(Anzahl_Follower)", "Partei*s(Struktur_4)",
-#               "Partei*s(Struktur_22)", "Partei*s(Struktur_42)", "Partei*s(Struktur_54)")
-
+prev_var <- c("Partei", "Bundesland", "Datum", "Struktur_4", "Struktur_22", "Struktur_42", "Struktur_54")
 outcome <- ""
 prevalence <- as.formula(paste(outcome, paste(prev_var, collapse = "+"), sep = "~")) 
 
-K <- 6
+K <- 15
 
 # fit model
 mod_prev <- stm::stm(
@@ -67,54 +72,87 @@ mod_prev <- stm::stm(
   vocab = vocab,
   data = meta,
   K = K,
-  prevalence = prevalence,
+  prevalence =~ Partei,
   gamma.prior = 'L1',
   seed = 123,
-  max.em.its = 200,
+  max.em.its = 350,
   init.type = "Spectral")
 
-# mod_prev$mu # corpus mean of topic prevalence and coefficients
-# mod_prev$beta # word probabilities for each topic
-# mod_prev$sigma # covariance matrix
-# mod_prev$theta # topic proportions
-
-# top words per topic (all topics)
-topic_words <- labelTopics(mod_prev)
-
-topic_number <- 2
-
-logbeta_matrix <- mod_prev$beta$logbeta[[1]]
-mod_prev$vocab[which.max(logbeta_matrix[topic_number,])] # highest prob (frequency) word for chosen topic_number
-
-# word clouds
-cloud(mod_prev, topic = topic_number, scale = c(2.5, 0.25))
-
-# table of MAP topic proportions per document (for all topics)
-topic_props <- make.dt(
-  mod_prev, 
-  data$meta[
-    c("Name", "Partei", "Bundesland")]
-) %>% cbind(docname = names(data$documents), .)
+# ----------------------------------------------------------------------------------------------
+# ---------------------------------------- Labelling -------------------------------------------
+# ----------------------------------------------------------------------------------------------
 
 # labeling workflow (for each topic): 
-## (1) inspect most frequent words per topic
+
+## (1) inspect most frequent words per topic (using different metrics as well as word cloud)
 ## (2) evaluate most representative documents per topic
 ## (3) assign label
 
-## preparation
+# ----------------------------------------------------------------------------------------------
+
+# first, prepare objects/variables needed for labelling process
+
+## table of MAP topic proportions per document (for all topics)
+topic_props <- make.dt(
+  mod_prev, 
+  data$meta[c("Name", "Partei", "Bundesland")]) %>% 
+  cbind(docname = names(data$documents), .)
+
+## top words per topic (for all topics)
+topic_words <- labelTopics(mod_prev, n = 8)
+
+## number of top documents to be printed in step (2)
 docs_number <- 5
 
+## initialize list with empty labels
+topic_labels <- list(
+  Topic1 = NULL,
+  Topic2 = NULL,
+  Topic3 = NULL,
+  Topic4 = NULL,
+  Topic5 = NULL,
+  Topic6 = NULL
+)
+
+# ----------------------------------------------------------------------------------------------
+
+# specify topic that should be labelled
+topic_number <- 6
+
+# ----------------------------------------------------------------------------------------------
+
+# actual labelling porcess
+
 ## (1) inspect most frequent words per topic
-topic_words$prob[topic_number,]
+topic_words$prob[topic_number,] # 20 most frequent words
+
+# logbeta_matrix <- mod_prev$beta$logbeta[[1]]
+# mod_prev$vocab[which.max(logbeta_matrix[topic_number,])] # most frequent word directly from (log)beta vector
 
 ## (2) evaluate most representative documents per topic
-data_corpus$docname <- paste0(data_corpus$Twitter_Username, "_", data_corpus$Jahr, "_", data_corpus$Monat) 
+topic_number_long <- paste0("Topic", topic_number)
+data_corpus$docname <- paste0(data_corpus$Twitter_Username, "_", data_corpus$Jahr, "_", data_corpus$Monat)
+
+repr_docs <-  topic_props %>%
+  arrange(desc(!!as.symbol(topic_number_long))) %>%
+  .[1:docs_number, c("Name", "docname", "Partei", topic_number_long)] %>%
+  left_join(data_corpus[,c("Tweets_Dokument", "docname")], 
+            by = "docname") %>%
+  mutate(Tweets_Dokument = substr(Tweets_Dokument, 50000, 52000))
+repr_docs[1,] # view i-th most representative document
+
+
 
 topic_props %>%
   arrange(desc(Topic2)) %>%
   .[1:docs_number,c("Name", "docname", "Partei", paste0("Topic", topic_number))] %>%
   left_join(select(data_corpus, "Tweets_Dokument", "docname"), by = "docname") %>%
   select(c("Name", "docname", "Partei", paste0("Topic", topic_number), "Tweets_Dokument"))
+
+
+
+## (3) assign label
+topic_labels[[topic_number]] <- "Green"
 
 ## (3) assign label
 topic_labels <- c("right/national",
@@ -123,6 +161,24 @@ topic_labels <- c("right/national",
                   "Europe",
                   "left/social",
                   "miscellaneous")
+
+
+# ----------------------------------------------------------------------------------------------
+
+# results
+
+# topic_labels <- list(
+#   Topic1 = "Miscellaneous",
+#   Topic2 = "Work & Family",
+#   Topic3 = "Left/Social",
+#   Topic4 = "Green/Miscellaneous",
+#   Topic5 = "Miscellaneous",
+#   Topic6 = "Green"
+# )
+
+# ----------------------------------------------------------------------------------------------
+# -------------------------------- Global Topic Inspection -------------------------------------
+# ----------------------------------------------------------------------------------------------
 
 # all topics w/ global proportions (prints topic words with their corpus frequency)
 plot(mod_prev, type = "summary", xlim = c(0, 0.3), custom.labels = topic_labels) # average thetas across all MPs 
@@ -134,15 +190,15 @@ while (i <= length(doc_lengths)) {
   weights[[i]] <- doc_lengths[[i]]/2
   i <- i + 1}
 mean_weight <- mean(weights)
-frequency_raw <- colMeans(mod_prev$theta[,1:K])
-frequency_weighted <- colMeans((mod_prev$theta*weights/mean_weight)[, 1:K])
+props_unweighted <- colMeans(mod_prev$theta[,1:K])
+props_weighted <- colMeans((mod_prev$theta*weights/mean_weight)[, 1:K])
 
-frequency_df <- data.frame(topic_labels, frequency_raw, frequency_weighted) %>% reshape2::melt(id = "topic_labels")
-colnames(frequency_df) <- c("topic", "variable", "frequency")
+props_df <- data.frame(topic_labels, props_unweighted, props_weighted) %>% reshape2::melt(id = "topic_labels")
+colnames(props_df) <- c("topic", "variable", "proportion")
 
-ggplot(data = frequency_df, aes(x = topic, y = frequency, fill = variable)) + 
+ggplot(data = props_df, aes(x = topic, y = proportion, fill = variable)) + 
   geom_bar(stat = "identity", position = "dodge") +
-  scale_fill_manual(values=c("grey50","grey80")) +
+  scale_fill_manual(values = c("grey40","grey80")) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, size = 12, hjust = 1),
         axis.title.x = element_text(size = 12, face = "bold"),
@@ -158,214 +214,3 @@ plot.topicCorr(mod_prev_corr)
 
 cormat <- cor(mod_prev$theta)
 cormat
-
-# metadata/topic relationship ---------------------------------------------
-covar <- c("Partei", "Bundesland")
-
-# factorize categorical variables, set CDU/CSU as reference category for variable "Partei"
-meta$Partei <- meta$Partei %>%
-  as.factor() %>%
-  relevel(ref = 3)
-meta$Bundesland <- as.factor(meta$Bundesland)
-
-# training data
-topic_props_train <- make.dt(
-  mod_prev, 
-  meta[covar])
-
-topic_props_train %>%
-  ggplot(aes(Partei, Topic2)) +
-  geom_boxplot(outlier.shape = NA) +
-  geom_point() +
-  labs(y = "Themenanteil", 
-       title = "'right/national' - topic proportion by party")
-
-topic_props_train %>%
-  ggplot(aes(Struktur_22,Topic4)) +
-  geom_smooth(method = "lm", formula = y ~ s(x)) +
-  labs(x = "BIP pro Kopf", y = "Themenanteil", 
-       title = "BIP vs. Thema 'Arbeit & Soziales'")
-
-## 
-outcome <- "1:6"
-prevalence <- as.formula(paste(outcome, paste(prev_var, collapse = "+"), sep = "~")) 
-
-prep <- stm::estimateEffect(1:6 ~ Partei + Bundesland + s(Anzahl_Follower) + 
-                              s(Struktur_4) + s(Struktur_22) + s(Struktur_42) + 
-                              s(Struktur_54),
-                            mod_prev,
-                            metadata = meta_train,
-                            uncertainty = "Global")
-summary(prep, topics = 1)
-
-### difference between two parties regarding topic prevalence
-plot(prep, "Partei", method = "difference", topics = c(1,2,3,4,5,6), cov.value1 = "CDU/CSU",
-     cov.value2 = "SPD", model = mod_prev, xlab = "more SPD          ...    more CDU/CSU",
-     labeltype = "custom", custom.labels = mod_prev_labels)
-
-### prevalence of topic 6 for all states
-plot(prep, "Bundesland", method = "pointestimate", topics = 6,
-     model = mod_prev, printlegend = FALSE, xaxt = "n", xlab = "Bundesland")
-
-### impact of immigrant share on prevalence of topic "left/social"
-plot(prep, "Struktur_4", method = "continuous", topics = 2,
-     model = mod_prev, printlegend = FALSE, xaxt = "n", xlab = "% immigrants",
-     ylim = c(0, 0.30))
-axis(1, tick = TRUE)
-
-### impact of immigrant share on prevalence of topic "right/national"
-plot(prep, "Struktur_4", method = "continuous", topics = 4,
-     model = mod_prev, printlegend = FALSE, xaxt = "n", xlab = "% immigrants",
-     ylim = c(0.05, 0.25))
-axis(1, tick = TRUE)
-
-### impact of immigrant share on prevalence of topic "green/climate"
-plot(prep, "Struktur_4", method = "continuous", topics = 6,
-     model = mod_prev, printlegend = FALSE, xaxt = "n", xlab = "% immigrants",
-     ylim = c(0, 0.15))
-axis(1, tick = TRUE)
-
-### impact of immigrant share on prevalence of topic "Europe"
-plot(prep, "Struktur_4", method = "continuous", topics = 3,
-     model = mod_prev, printlegend = FALSE, xaxt = "n", xlab = "% immigrants",
-     ylim = c(0.1, 0.25))
-axis(1, tick = TRUE)
-
-# prevalence model w/ interaction -----------------------------------------
-
-# fit model
-mod_prev_int <- stm::stm(
-  documents = docs,
-  vocab = vocab,
-  data = meta,
-  K = 6,
-  prevalence =~ Partei + Bundesland + Struktur_4
-  + Struktur_22 + Struktur_42 + Struktur_54 + Partei*Struktur_42,
-  max.em.its = 75,
-  init.type = "Spectral")
-
-### interaction party and unemployment for left/social topic
-covariate <- "Struktur_42"
-x_axis_label <- "Unemployment (in %)"
-moderator <- "Partei"
-
-prep <- estimateEffect(c(2) ~ Partei * Struktur_42, mod_prev_int,
-                       metadata = meta, uncertainty = "None")
-
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "CDU/CSU", linecol = "black", ylim = c(0, 0.70),
-     printlegend = FALSE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "SPD", linecol = "red", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "Bündnis 90/Die Grünen", linecol = "green", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "Die Linke", linecol = "purple", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "FDP", linecol = "yellow", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "AfD", linecol = "blue", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-
-### interaction party and GDP per capita for left/social topic
-covariate <- "Struktur_22"
-x_axis_label <- "GDP per capita"
-moderator <- "Partei"
-
-prep <- estimateEffect(c(2) ~ Partei * Struktur_22, mod_prev_int,
-                       metadata = meta, uncertainty = "None")
-
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "CDU/CSU", linecol = "black", ylim = c(0, 0.70),
-     printlegend = FALSE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "SPD", linecol = "red", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "Bündnis 90/Die Grünen", linecol = "green", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "Die Linke", linecol = "purple", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "FDP", linecol = "yellow", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "AfD", linecol = "blue", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-
-### interaction party and percentage of immigrants for left/social topic
-covariate <- "Struktur_4"
-x_axis_label <- "immigrant percentage"
-moderator <- "Partei"
-
-prep <- estimateEffect(c(3) ~ Partei * Struktur_4, mod_prev_int,
-                       metadata = meta, uncertainty = "None")
-
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "CDU/CSU", linecol = "black", ylim = c(0, 0.70),
-     printlegend = FALSE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "SPD", linecol = "red", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "Bündnis 90/Die Grünen", linecol = "green", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "Die Linke", linecol = "purple", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "FDP", linecol = "yellow", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-plot(prep, covariate = covariate, model = mod_prev_int,
-     method = "continuous", xlab = x_axis_label, moderator = moderator,
-     moderator.value = "AfD", linecol = "blue", ylim = c(0, 0.70),
-     printlegend = FALSE, add = TRUE)
-
-
-# topical content -------------------------------------------------------
-
-mod_topic <- stm::stm(
-  documents = docs,
-  vocab = vocab, 
-  data = meta,
-  K = 6,
-  prevalence =~ Partei + Bundesland + s(Struktur_4)
-  + s(Struktur_22) + s(Struktur_42) + s(Struktur_54),
-  content =~ Partei,
-  max.em.its = 75,
-  init.type = "Spectral")
-
-### top words per topic
-labelTopics(mod_topic, n = 10)
-
-### difference between two parties and one topic regarding vocabulary usage
-plot(mod_topic, type = "perspectives", topics = 2)
-
-### all topics with overall proportions
-plot(mod_topic, type = "summary", xlim = c(0, 0.3))
-
-### word clouds
-cloud(mod_prev, topic = 6, scale = c(2, 0.25), labeltype = "prob")
-
